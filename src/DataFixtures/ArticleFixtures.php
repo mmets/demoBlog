5d6453f3d17ffbd71f8f2831585b717c13f8d049
Stéphanie <metm.stef@gmail.com>
2020-06-29 17:23:58 +0200

    nouvelles entites et affichage commentaires

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,7 +2,10 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -12,40 +15,89 @@ class ArticleFixtures extends Fixture
     // l'interface ObjectManager et son objet $manager 
 
     public function load(ObjectManager $manager)
-    {   // ObjectManager est une classe qui permet de 
-        // manipuler, gérer les insertions en bdd
+    {   
+        /* La libriaire FAKER permet d'injecter de fausses données 
+        plus spécifiques comme des paragraphes ou des noms
+        */
+        $faker = \Faker\Factory::create('fr_FR') ;
 
-        // La classe ObjectManager est une classe prédéfinie en Symfony qui permet de manipuler les lignes de la BDD (INSERT, UPDATE, DELETE)
-
-
-        // On va créer une boucle qui génère 10 articles 
-
-        for ($i=1; $i <= 10 ; $i++) 
+        // Création de 3 catégories
+        for ($i=1; $i < 3; $i++) 
         { 
-            // On instancie la classe Article qui cependant n'existe pas
-            // Grâce à l'extension php namespace resolver, on va pouvoir 
-            // importer les classes automatiquement du dossier entité/article.php
-            // Pour importer les classes on peut cliquer sur la classe à importer 
-            // puis faire Ctrl+Alt+i ou clic droit>import class
+            /*
+                On instancie la classe Category afin de lui affecter des 
+                valeurs issues de la librairie FAKER via les setter 
 
+                $manager->persist($category) prépare les données pour 
+                une insertion en BDD
+            */
+            $category = new Category;
+    
+            $category->setTitre($faker->sentence())
+                      ->setDescription($faker->paragraph());
 
+            $manager->persist($category);
 
-            $article = new Article();// Lorsqu'une classe n'a pas d'argument, on peut ommettre les ()
+            // Création de 4 à 6 articles par catégorie
+            for ($j=1; $j <= mt_rand(4,6) ; $j++) 
+            { 
+                /* 
+                    On instancie ensuite l'entité Article afin d'insérer 
+                    des données en BDD via les différents setter
 
-            $article->setTitle("Titre de l'article n° $i")
-                    ->setText("Voici venu le jour n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("https://picsum.photos/200")
-                    ->setCreatedAt(new \DateTime());// l'antislash permet de revenir dans l'espace global le temps de la ligne
-                    // car DateTime est une classe de php non définie dans ArticleFixtures 
-                    // Il est possible d'utiliser use pour dire dans quel namespace on utilise DateTime
+                    Dans le cas de setContent, on lui définit une variable 
+                    $content qui va stocker les paragraphes fictifs concatenés
 
-            $manager->persist($article);// prépare les requêtes d'insertion
-            // persist() est une méthode issue de la classe ObjectManager qui permet de préparer les insertions et de les garder en mém
+                    Pour ce qui est de la date, on définit un intervalle de 
+                    telle sorte que la date de création de l'article ne puisse 
+                    pas être dans le futur
 
+                    setCategory($category) permet de relier les articles aux 
+                    catégories correspondantes
+                */
+                $article = new Article;
 
+                $content = '<p>'. join($faker->paragraphs(5),'</p><p>') .'</p>';
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setText($faker->sentence())
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+
+                $manager->persist($article);
+
+                // Création de 4 à 10 articles par catégorie
+                for ($k=1; $k <= mt_rand(4,10) ; $k++) 
+                { 
+                    /*
+                        On instancie l'entité Comment afin d'insérer des commentaires 
+                        Ici, on s'assure que la date de création des commentaires ne 
+                        peut pas être antérieure à la date de création de l'article
+                    */
+                    $comment = new Comment;
+
+                    $content = '<p>'. join($faker->paragraphs(5),'</p><p>') .'</p>';
+
+                    $now = new \Datetime;
+                    $interval = $now->diff($article->getCreatedAt()); // représente la
+                    // différence de temps entre la date de créattion de l'article et 
+                    // maintenant ($now)
+                    $days = $interval->days; // nombre de jours entre la date de créa de 
+                    // l'article et maintenant
+                    $minimum = '-'. $days .'days';
+                    $comment->setAuthor($faker->name())
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimum))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                        
+                }
+            }
         }
+        $manager->flush();
 
-        $manager->flush();// permet d'exécuter la requête d'insertion
     }
 }
